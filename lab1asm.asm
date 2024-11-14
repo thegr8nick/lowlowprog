a1db4591ba313428a3dea2b73a31d11c88e88fcd
thegr8nick <kudashevnikitushka@yandex.ru>
2024-11-14 13:59:06 +0400

    upg asm

--- lab1asm.asm
+++ lab1asm.asm
@@ -1,26 +1,22 @@
+%include "io64.inc"
+
 section .data
     prompt db "Enter a number: ", 0
-    output_prime db "%d A prime number", 10, 0
-    output_not_prime db "%d Not a prime number.", 10, 0
-    format_input db "%d", 0
+    ent db "", 10, 0
+    output_prime db "A prime number", 10, 0
+    output_not_prime db "Not a prime number", 10, 0
 
 section .bss
     number resd 1
     is_prime resb 1
 
 section .text
-    extern printf, scanf
     global main
-
+    
 main:
-    push prompt
-    call printf
-    add esp, 4
+    PRINT_STRING prompt
 
-    push number
-    push format_input
-    call scanf
-    add esp, 8
+    GET_DEC 4, number
 
     mov eax, [number]
     cmp eax, 1
@@ -51,21 +47,19 @@ not_prime:
 
 end_check:
     mov eax, [number]
-    push eax
+
     cmp byte [is_prime], 1
     je print_prime
 
-    push eax
-    push output_not_prime
-    call printf
-    add esp, 8
+    PRINT_DEC 4, number
+    PRINT_STRING ent
+    PRINT_STRING output_not_prime
     jmp end_program
 
 print_prime:
-    push eax
-    push output_prime
-    call printf
-    add esp, 8
+    PRINT_DEC 4, number
+    PRINT_STRING ent
+    PRINT_STRING output_prime
 
 end_program:
     ret
