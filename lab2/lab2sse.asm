eca1746f7be5ee6cfe38c1c10b261606cfff7efe
thegr8nick <kudashevnikitushka@yandex.ru>
2024-12-18 21:19:47 +0400

    fix more

--- lab2/lab2sse.asm
+++ lab2/lab2sse.asm
@@ -2,8 +2,6 @@
 
 section .data
     real_numb: dd 3.3
-    round_up: dd 0.0
-    round_down: dd 0.0
 
 section .text
     global main
@@ -12,15 +10,14 @@ main:
     movss xmm0, dword [real_numb]
     roundss xmm0, xmm0, 0x01
     cvttss2si eax, xmm0
-    PRINT_STRING "Round up: "
+    PRINT_STRING "Round down: "
     PRINT_DEC 4, eax
     NEWLINE
 
     movss xmm0, dword [real_numb]
     roundss xmm0, xmm0, 0x02
-    movss dword [round_down], xmm0
     cvttss2si eax, xmm0
-    PRINT_STRING "Round down: "
+    PRINT_STRING "Round up: "
     PRINT_DEC 4, eax
 
 end:
