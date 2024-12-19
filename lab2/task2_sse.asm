338541bb9da042ca08e0f523c00f96d68f6902fb
thegr8nick <kudashevnikitushka@yandex.ru>
2024-12-20 00:28:39 +0400

    t2

--- lab2/task2_sse.asm
+++ lab2/task2_sse.asm
@@ -6,27 +6,54 @@ section .rodata
     b: dd 2.0
     c: dd 2.0
 
-    ;y = (b*c)/(a+x)
+    ;y = asin(bx) + cos(cx)
+
+section .bss
+    temp resd 1
     
 section .text
-global main
+    global main
 
 main:
-mov rbp, rsp
-xor eax, eax
+    mov rbp, rsp
+    xor eax, eax
+    
+    fld dword[b]
+    fld dword[x]
+    fmulp
+    fsin
+    fld dword [a]
+    fmulp
+    
+    fld dword[b]
+    fld dword[x]
+    fmulp
+    fcos
+    
+    fadd
 
-FLD dword[b] ;ST0 = b
-FLD dword[c] ;ST1 = c
-FMULP ;ST0 = b*c
-FLD dword[a] 
-FLD dword[x] 
-FADDP
-FDIVP
 
-movss xmm0, dword[c]
-mulss xmm0, [b]
-movss xmm1, dword[a]
-addss xmm1, [x]
-divss xmm0, xmm1
+    movss xmm0, dword [x]
+    mulss xmm0, dword [b]
+    movss dword [temp], xmm0
 
-ret
+    fld dword [temp]
+    fsin
+    fstp dword [temp]
+
+    movss xmm0, dword [temp]
+    mulss xmm0, dword [a]
+    
+    movss xmm1, dword [x]
+    mulss xmm1, dword [c]
+    movss dword [temp], xmm1
+    
+    fld dword [temp]
+    fcos
+    fstp dword [temp]
+    
+    movss xmm1, dword [temp]
+    addss xmm0, xmm1
+
+    
+    ret
