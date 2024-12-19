189eab236678b2acaeb91134b75ad007603e5dc1
thegr8nick <kudashevnikitushka@yandex.ru>
2024-12-19 23:35:26 +0400

    task 2

--- lab2/task2_sse.asm
+++ lab2/task2_sse.asm
@@ -1,54 +1,32 @@
+%include "io64.inc"
+
 section .rodata
-    x: dd -0.5
-    num: dd 30
-    ln2: dd 0.6931471805599
+    a: dd 1.0
+    x: dd 1.0
+    b: dd 2.0
+    c: dd 2.0
 
-section .bss
-    result: resd 1
-
+    ;y = (b*c)/(a+x)
+    
 section .text
-    global main
-    
+global main
+
 main:
-    mov rbp, rsp; for correct debugging
-    movss xmm0, dword[x]
-    movss xmm2, dword[ln2]
-    xorps xmm1, xmm1
-    mov ecx, 1
-    
-.start_loop:
-    cmp ecx, dword[num]
-    jge .end_loop
-    mov eax, ecx
-    cvtsi2ss xmm3, ecx
-    movss xmm5, xmm0
+mov rbp, rsp
+xor eax, eax
 
-.loop_power:
-    cmp eax, 1
-    jle .skip_power
-    mulss xmm5, xmm0
-    dec eax
-    jmp .loop_power
-    
-.skip_power:
+FLD dword[b] ;ST0 = b
+FLD dword[c] ;ST1 = c
+FMULP ;ST0 = b*c
+FLD dword[a] 
+FLD dword[x] 
+FADDP
+FDIVP
 
-    movss xmm4, xmm5
-    divss xmm4, xmm3
+movss xmm0, dword[c]
+mulss xmm0, [b]
+movss xmm1, dword[a]
+addss xmm1, [x]
+divss xmm0, xmm1
 
-    test ecx, 1
-    jnz .add_term
-    subss xmm1, xmm4
-    jmp .next_iter
-    
-.add_term:
-    addss xmm1, xmm4
-
-.next_iter:
-    inc ecx
-    jmp .start_loop
-    
-.end_loop:
-    divss xmm1, xmm2
-    movss dword[result], xmm1
-    
-    ret
+ret
