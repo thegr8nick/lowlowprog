1e4b9dd0596f2ec2bbd5378b125140e14d7efc48
thegr8nick <kudashevnikitushka@yandex.ru>
2024-12-19 21:26:16 +0400

    upg inf

--- lab2/task3.asm
+++ lab2/task3.asm
@@ -1,40 +1,28 @@
-section .data
-    result dd 0.0
-    a dd 1.0
-    b dd 0.0
+%include "io64.inc"
 
+section .bss
+    x: resd 1
+section .rodata
+    a: dd 3.0
+    section .rodata
+    ;2^ctg(x) = a
+    ;x = arctg(log2(a))
+    
 section .text
-    global main
-    extern exit
+global main
 
 main:
-    ; x = arctan(2^b) - a
+mov rbp, rsp;
+xor rbx, rbx
 
-    ; Вычисляем 2^b
-    fld dword [b]             ; st(0) = b
-    fld1                      ; st(1) = 1.0 (логарифмическое основание)
-    fyl2x                     ; st(0) = b * log2(2) = b (так как log2(2) = 1)
+fld1
+fld dword[a] ;ST0 = x
 
-    ; Преобразуем в степень: 2^b
-    fld1                      ; st(1) = 1.0
-    fld st1                   ; st(2) = 1.0, st(1) = 1.0
-    fprem                     ; st(0) = дробная часть st(1) (для корректировки диапазона)
-    f2xm1                     ; st(0) = 2^(дробная часть) - 1
-    fadd                      ; st(0) = 2^(дробная часть)
-    fscale                    ; st(0) = 2^b
-    fstp st1                  ; Удаляем старые значения, оставляем только результат в st(0)
+fyl2x
+fld1
+fxch
+fpatan
 
-    ; Вычисляем arctan(2^b)
-    fld1                      ; st(1) = 1.0
-    fpatan                    ; st(0) = arctan(2^b)
+fstp dword[x]
 
-    ; Вычитаем a
-    fsub dword [a]            ; st(0) = arctan(2^b) - a
-
-    ; Сохраняем результат в переменную result
-    fstp dword [result]       ; Сохраняем результат в памяти
-
-    ; Завершаем программу
-    xor rdi, rdi              ; Код завершения = 0
-    call exit                 ; Вызов exit
-    ret
+ret
